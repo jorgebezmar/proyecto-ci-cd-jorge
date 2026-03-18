5a9a3435bda5ece6f34f7d7c4c1b14d34ed24072
Jorge Bezares <pcjorgeb@gmail.com>
2026-03-18 13:00:02 +0100

    prueba final video

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola, soy Jorge y esta es la prueba para el video!"
+    return "Hola, soy Jorge y esta es la prueba para el video"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
